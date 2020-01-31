39301f0d695d472f9205efa89b4dccd60e6129c5
Rocco <graphic@IMM-Instructor.local>
2020-01-31 13:59:13 -0500

    Outline all actions

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// EVT: Capture a click from the .signin
+
+// UI: Set .modal style for display to block
+
+// EVT: Capture a click from the .close
+
+// UI: Set .modal style for display to none
+
+// EVT: Capture a click from .submit
+
+// UI: Add .error class to #user 
+
+// UI: Add .error class to #pass 
+
+// EVT: Capture focus from #user
+
+// UI: Remove .error class from #user 
+
+// EVT: Capture focus from #pass
+
+// UI: Remove .error class from #pass 
