7a4b4bdc88d66112f3c9cad960458a4b3d373bac
Rocco <graphic@IMM-Instructor.local>
2020-01-31 14:17:01 -0500

    Setup process

--- js/main.js
+++ js/main.js
@@ -1,21 +1,35 @@
-// EVT: Capture a click from the .signin
+// ***** DOCUMENT REFERENCES *****
+let $modal =   document.querySelector(`.modal`)
+let $signin =  document.querySelector('.signin')
+
+
+
+// ***** INTERFACE MODIFICATIONS *****
 
 // UI: Set .modal style for display to block
-
-// EVT: Capture a click from the .close
+// $modal.style.display = `block`
 
 // UI: Set .modal style for display to none
-
-// EVT: Capture a click from .submit
 
 // UI: Add .error class to #user 
 
 // UI: Add .error class to #pass 
 
-// EVT: Capture focus from #user
-
 // UI: Remove .error class from #user 
 
-// EVT: Capture focus from #pass
-
 // UI: Remove .error class from #pass 
+
+
+
+// ***** USER INTERACTIONS *****
+
+// EVT: Capture a click from the .signin
+$signin.addEventListener('click', event => {  })
+
+// EVT: Capture a click from the .close
+
+// EVT: Capture a click from .submit
+
+// EVT: Capture focus from #user
+
+// EVT: Capture focus from #pass
